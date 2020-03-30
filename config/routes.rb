1cc52b5f2c1f1a7c57188f74355cf15da76addb0
nb <bgsnorfa@gmail.com>
2020-03-30 17:26:56 +0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :shorteners, only: [:index, :create]
+      post '/menu', to: 'shorteners#menu'
     end
   end
 end
